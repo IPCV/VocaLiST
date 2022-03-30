93df59bebc9962aa56a20a32add5ad11a5b1148a
kvsphantom <kvsphantom@gmail.com>
2022-03-30 13:07:41 +0200

    index page fixed

--- _site/estimated_demo/plot_spec.py
+++ _site/estimated_demo/plot_spec.py
@@ -11,22 +11,21 @@ from pathlib import Path
 NFFT = 1022
 HOP_LENGTH = 256
 TARGET_SAMPLING_RATE = 16384
-wav_files = Path('.').rglob('*.wav')
+wav_files = Path('/home/venkatesh/Downloads/venkatesh_interspeech').rglob('*.wav')
 for file in wav_files:
     file_path = file.as_posix()
-    if ('000049' in file_path or '000029' in file_path):
-        file_name = os.path.basename(file_path)
-        folder_path = os.path.dirname(file_path)
-        output_path = os.path.join(folder_path, file_name[:-4] + '.png')
-        y, sr = librosa.load(file_path, sr=16384)
-        sound = pydub.AudioSegment.from_wav(file_path)
-        sound.export(file_path[:-4]+'.mp3', format="mp3")
+    file_name = os.path.basename(file_path)
+    folder_path = os.path.dirname(file_path)
+    output_path = os.path.join(folder_path, file_name[:-4] + '.png')
+    y, sr = librosa.load(file_path, sr=16384)
+    sound = pydub.AudioSegment.from_wav(file_path)
+    sound.export(file_path[:-4]+'.mp3', format="mp3")
 
-        stft = librosa.stft(y, n_fft=NFFT, hop_length=HOP_LENGTH)
-        D = librosa.amplitude_to_db(np.absolute(stft),
-                                    ref=np.max)
-        librosa.display.specshow(D, sr=sr, hop_length=HOP_LENGTH, x_axis='time', cmap='Reds', y_axis='linear')
-        plt.ylabel(None)
-        plt.xlabel(None)
-        plt.savefig(output_path, bbox_inches='tight')
-        #plt.show()
+    stft = librosa.stft(y, n_fft=NFFT, hop_length=HOP_LENGTH)
+    D = librosa.amplitude_to_db(np.absolute(stft),
+                                ref=np.max)
+    librosa.display.specshow(D, sr=sr, hop_length=HOP_LENGTH, x_axis='time', cmap='Reds', y_axis='linear')
+    plt.ylabel(None)
+    plt.xlabel(None)
+    plt.savefig(output_path, bbox_inches='tight')
+    #plt.show()
